ef97ade595e7ebc45e798d0d622798822ef572f5
SandyArdiyan <sanardiyan881@gmail.com>
2025-10-17 16:44:56 +0700

    Box dan Column

--- app/src/main/java/com/example/latihan/Tataletak.kt
+++ app/src/main/java/com/example/latihan/Tataletak.kt
@@ -1,17 +1,27 @@
 package com.example.latihan
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TatakletakColumn(modifier: Modifier) {
@@ -99,3 +109,75 @@ fun TataletakRow2(modifier: Modifier) {
     }
 }
 
+
+
+@Composable
+fun TataLetakBoxColumnRow(modifier: Modifier) {
+    val gambar = painterResource(id = R.drawable.kucing)
+
+    Column {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(110.dp)
+                .background(color = Color.Yellow),
+            contentAlignment = Alignment.Center
+        ) {
+            // Isi Box
+            Image(
+                painter = gambar,
+                contentDescription = "kucing"
+            )
+        }
+
+        Column {
+            Row(
+                modifier = modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(text = "Col1_Row1_Komponen1")
+                Text(text = "Col1_Row1_Komponen2")
+                Text(text = "Col1_Row1_Komponen3")
+            }
+
+            Row(
+                modifier = modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(text = "Col1_Row2_Komponen1")
+                Text(text = "Col1_Row2_Komponen2")
+                Text(text = "Col1_Row2_Komponen3")
+            }
+            Row(
+                modifier = modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(text = "Col1_Row3_Komponen1")
+                Text(text = "Col1_Row3_Komponen2")
+                Text(text = "Col1_Row3_Komponen3")
+            }
+        }
+    }
+    Spacer(Modifier.height(100.dp))
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(110.dp)
+            .background(color = Color.Yellow),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = gambar,
+            contentDescription = null,
+            contentScale = ContentScale.Fit)
+        Text(text = "My kucing",
+                    fontSize = 50.sp,
+            color = Color.Red,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Cursive,
+            modifier = Modifier.align(
+                alignment = Alignment.Center
+            )
+        )
+    }
+}
