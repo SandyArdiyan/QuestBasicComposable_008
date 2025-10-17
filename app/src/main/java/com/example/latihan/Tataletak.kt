055c3946692fcb24d2478e4ba59b70049139c6df
SandyArdiyan <sanardiyan881@gmail.com>
2025-10-17 15:53:53 +0700

    Bikin Kompenen

--- app/src/main/java/com/example/latihan/Tataletak.kt
+++ app/src/main/java/com/example/latihan/Tataletak.kt
@@ -1,11 +1,18 @@
 package com.example.latihan
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun TatakletakColumn(modifier: Modifier: Modifier) {
-    Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
+fun TatakletakColumn(modifier: Modifier) {
+    Column (modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
         Text(text = "Komponen1")
         Text(text = "Komponen2")
         Text(text = "Komponen3")
@@ -14,22 +21,15 @@ fun TatakletakColumn(modifier: Modifier: Modifier) {
 }
 
 @Composable
-fun TataletakBox (modifier: Modifier) {
-    Box (
-        modifier = modifier
-            .fillMaxHeight()
-            .fillMaxWidth(), contentAlignment = Alignment.Center
-
-    )  {
-        Text(text = "Box 1")
-        Text(text = "Colum 1")
-        Text(text = "Row 1")
-        Text(text = "Box 2")
-        Text(text = "Column 2")
+fun TataletakBox(modifier: Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
     }
 }
-
-
-
-
 
